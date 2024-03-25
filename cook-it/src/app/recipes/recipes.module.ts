b7129f1a98c58e379e168a57e83fac65a4773567
SissyManson <komandira123@gmail.com>
2024-03-25 17:16:20 +0200

    Created static recipe card

---
+++ cook-it/src/app/recipes/recipes.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecipeCardComponent } from './recipe-card/recipe-card.component';
+
+
+
+@NgModule({
+  declarations: [
+    RecipeCardComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class RecipesModule { }
