7eca31a1427a19406cb483710d280940c372d333
SissyManson <komandira123@gmail.com>
2024-03-25 17:34:49 +0200

    Created recipe components

--- cook-it/src/app/recipes/recipes.module.ts
+++ cook-it/src/app/recipes/recipes.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RecipeCardComponent } from './recipe-card/recipe-card.component';
+import { CreateComponent } from './create/create.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailsComponent } from './details/details.component';
+import { AllRecipesComponent } from './all-recipes/all-recipes.component';
 
 
 
 @NgModule({
   declarations: [
-    RecipeCardComponent
+    RecipeCardComponent,
+    CreateComponent,
+    EditComponent,
+    DetailsComponent,
+    AllRecipesComponent
   ],
   imports: [
     CommonModule
