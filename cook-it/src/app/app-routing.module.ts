ee9767c270218b113615d9ea9c9e6182b277cf97
SissyManson <komandira123@gmail.com>
2024-03-25 16:53:24 +0200

    Added 404page and routing

--- cook-it/src/app/app-routing.module.ts
+++ cook-it/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Page404Component } from './shared/page-404/page-404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/not-found' },
+  { path: 'not-found', component: Page404Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- cook-it/src/app/shared/shared.module.ts
+++ cook-it/src/app/shared/shared.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Page404Component } from './page-404/page-404.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    Page404Component
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [Page404Component],
+  imports: [CommonModule, RouterModule],
 })
-export class SharedModule { }
+export class SharedModule {}
