611809d56b76f1439f9a2eccb2d30f3a2fbe8d35
SissyManson <komandira123@gmail.com>
2024-04-01 17:26:44 +0300

    Updated recipe-card component

--- cook-it/src/app/recipes/recipe-card/recipe-card.component.ts
+++ cook-it/src/app/recipes/recipe-card/recipe-card.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from 'src/app/types/Recipe';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
-  styleUrls: ['./recipe-card.component.css']
+  styleUrls: ['./recipe-card.component.css'],
 })
 export class RecipeCardComponent {
-
 }
