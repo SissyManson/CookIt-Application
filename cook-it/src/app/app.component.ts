cb6aaa3a95cecebc0f5040f7d80d811cf19d570e
SissyManson <komandira123@gmail.com>
2024-04-01 17:23:32 +0300

    Header and footer now hide in login/register pages

--- cook-it/src/app/app.component.ts
+++ cook-it/src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cookIt';
+
+  hideHeadAndFooter: boolean = true;
+
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if (event instanceof NavigationStart) {
+        if (
+          event['url'] === '/auth/login' ||
+          event['url'] === '/auth/register'
+        ) {
+          // ! hide header and footer on login/register
+          this.hideHeadAndFooter = false;
+        } else {
+          this.hideHeadAndFooter = true;
+        }
+      }
+    });
+  }
 }
