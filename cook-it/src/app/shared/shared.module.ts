f0aee7b8c7a1a21b076556099d6723aac154efa5
SissyManson <komandira123@gmail.com>
2024-04-02 22:21:28 +0300

    Updated shared module

--- cook-it/src/app/shared/shared.module.ts
+++ cook-it/src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Page404Component } from './page-404/page-404.component';
 import { RouterModule } from '@angular/router';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [Page404Component],
+  declarations: [LoaderComponent, Page404Component],
   imports: [CommonModule, RouterModule],
+  exports: [LoaderComponent, Page404Component],
 })
 export class SharedModule {}
