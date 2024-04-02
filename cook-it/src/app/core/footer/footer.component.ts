d4683c9dce1df8f9f14f2622b057d7fc2f5a103d
SissyManson <komandira123@gmail.com>
2024-04-02 22:47:33 +0300

    Updated quick links in footer

--- cook-it/src/app/core/footer/footer.component.ts
+++ cook-it/src/app/core/footer/footer.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  constructor(private userService: UserService) {}
 
+  get isLoggedUser(): boolean {
+    return this.userService.isLoggedUser;
+  }
 }
