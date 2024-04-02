aab649fdc781344c3bcae4fa4190dd93872d3324
SissyManson <komandira123@gmail.com>
2024-04-02 22:26:14 +0300

    Added error component to routing

--- cook-it/src/app/app-routing.module.ts
+++ cook-it/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { Page404Component } from './shared/page-404/page-404.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { ErrorComponent } from './core/error/error.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', title: 'Home page', component: HomeComponent },
   { path: 'contacts', title: 'Contacts', component: ContactsComponent },
+  { path: 'error', component: ErrorComponent },
   { path: '**', redirectTo: '/not-found' },
-  { path: 'not-found',title: 'Page Not Found', component: Page404Component },
+  { path: 'not-found', title: 'Page Not Found', component: Page404Component },
 ];
 
 @NgModule({
