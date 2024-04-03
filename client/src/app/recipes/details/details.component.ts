3a0fd0b114f331158b41b67cd817f4a4ef33618f
SissyManson <komandira123@gmail.com>
2024-04-03 22:20:46 +0300

    Added detailed view for the recipes

--- client/src/app/recipes/details/details.component.ts
+++ client/src/app/recipes/details/details.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from 'src/app/types/Recipe';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  recipe = {} as Recipe;
+  totalTime: number = 0;
+  getDirections: Array<string> = [];
 
+  constructor(
+    private recipeService: RecipeService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    // get the recipe trough the id in the link
+    this.activatedRoute.params.subscribe((data) => {
+      this.recipeService.getOne(data['recipeId']).subscribe((recipe) => {
+        this.recipe = recipe;
+
+        this.recipe.directions = recipe.directions[0];
+        this.totalTime = recipe.prepTime + recipe.cookTime;
+        this.getDirections = this.recipe.directions.split('\n');
+      });
+    });
+  }
+
+  getTotalTime() {
+    if (this.totalTime >= 60) {
+      let hours = Math.floor(this.totalTime / 60);
+      let mins = this.totalTime % 60;
+      return `${hours} h ${mins} mins`;
+    }
+    return `${this.totalTime} mins`;
+  }
 }
