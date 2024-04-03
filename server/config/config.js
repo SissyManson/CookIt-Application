0d222aaeebf37023efced9074bf792999a926410
SissyManson <komandira123@gmail.com>
2024-04-03 14:55:30 +0300

    Updated DB connection string

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
    development: {
       port: process.env.PORT || 3030,
-      dbURL: 'mongodb+srv://sdancheva9:%24uPer%24Trong_P%40ssw0rd@cook-it.5coiwyw.mongodb.net/?retryWrites=true&w=majority&appName=cook-it',
+      dbURL: 'mongodb+srv://sdancheva9:%24uPer%24Trong_P%40ssw0rd@cook-it.5coiwyw.mongodb.net/',
       origin: ['http://localhost:4200', 'http://localhost:5555'],
    },
    production: {
