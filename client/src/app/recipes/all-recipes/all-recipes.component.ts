a41fe45b7e2d7f26f552788bee32bb99fa286cfc
SissyManson <komandira123@gmail.com>
2024-04-03 22:19:10 +0300

    Created recipes are now visible

--- client/src/app/recipes/all-recipes/all-recipes.component.ts
+++ client/src/app/recipes/all-recipes/all-recipes.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-all-recipes',
   templateUrl: './all-recipes.component.html',
-  styleUrls: ['./all-recipes.component.css']
+  styleUrls: ['./all-recipes.component.css'],
 })
-export class AllRecipesComponent {
-
-}
+export class AllRecipesComponent {}
