6f2ffa80284ef6c2fbd84412a12fe5a31f95947d
SissyManson <komandira123@gmail.com>
2024-04-03 22:22:17 +0300

    Exporting the slice-pipe

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { Page404Component } from './page-404/page-404.component';
 import { RouterModule } from '@angular/router';
 import { LoaderComponent } from './loader/loader.component';
+import { SlicePipe } from './slice.pipe';
 
 @NgModule({
-  declarations: [LoaderComponent, Page404Component],
+  declarations: [LoaderComponent, Page404Component, SlicePipe],
   imports: [CommonModule, RouterModule],
-  exports: [LoaderComponent, Page404Component],
+  exports: [LoaderComponent, Page404Component, SlicePipe],
 })
 export class SharedModule {}
