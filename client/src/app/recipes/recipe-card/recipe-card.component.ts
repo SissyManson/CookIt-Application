4c6b865fd030dc22814ca0db43f8726d2b240514
SissyManson <komandira123@gmail.com>
2024-04-03 22:23:37 +0300

    Edited recipe card - new view
    Recipe card now works with dynamic data

--- client/src/app/recipes/recipe-card/recipe-card.component.ts
+++ client/src/app/recipes/recipe-card/recipe-card.component.ts
@@ -1,10 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from 'src/app/types/Recipe';
+import { RecipeService } from '../recipe.service';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.css'],
 })
-export class RecipeCardComponent {
+export class RecipeCardComponent implements OnInit {
+  recipes: Recipe[] | null = [];
+  isLoading: boolean = true;
+
+  constructor(
+    private recipeService: RecipeService,
+    private userService: UserService
+  ) {}
+
+  get isLoggedUser(): boolean {
+    return this.userService.isLoggedUser;
+  }
+
+  get userId(): string {
+    return this.userService.user?._id || '';
+  }
+
+  
+  // ! TODO: textarea new lines are saved as '\n'
+  ngOnInit(): void {
+    this.recipeService.getAll().subscribe((recipess) => {
+      // console.log(recipess[0].directions[0]);
+
+      this.recipes = recipess;
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1000);
+    });
+  }
 }
